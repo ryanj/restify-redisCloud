2a41eab98128cbc22eac703ec120de1ca4ee02c6
ryanj <ryan.jarvinen@gmail.com>
2013-11-12 10:00:04 -0800

    adding HTML usage info, documenting API endpoints

--- server.js
+++ server.js
@@ -19,18 +19,27 @@ app.use(restify.fullResponse())
 
 // Routes
 app.get('/set', function (req, res, next){
+  // Set a value
   var result = client.set('welcome_msg', "Hello World");
   res.send(result);
 });
 app.get('/get', function (req, res, next){
-  var result = client.get('welcome_msg');
-  res.send(result);
+  client.get("welcome_msg", function (err, reply) {
+    // When defined, will print "Hello World!"
+    if(err){
+      console.log(err);
+    }else{
+      res.send(reply.toString()); 
+    }
+  });
 });
 app.get('/info', function (req, res, next){
+  // Get DB instance info
   var result = client.server_info;
   res.send(result);
 });
 app.get('/flush', function (req, res, next){
+  // Flush all DB data
   var result = client.flushall();
   res.send(result);
 });
@@ -40,6 +49,7 @@ app.get('/status', function (req, res, next)
   res.send("{status: 'ok'}");
 });
 
+//Serve our index page
 app.get('/', function (req, res, next)
 {
   var data = fs.readFileSync(__dirname + '/index.html');
@@ -47,7 +57,7 @@ app.get('/', function (req, res, next)
   res.header('Content-Type', 'text/html');
   res.end(data.toString().replace(/host:port/g, req.header('Host')));
 });
-
+//Static files
 app.get(/\/(css|js|img)\/?.*/, restify.serveStatic({directory: './static/'}));
 
 app.listen(config.port, config.ip, function () {

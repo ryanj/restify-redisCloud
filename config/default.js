d7a95f5ed5a60247ddcae9d3dd8a75a5dd01b065
ryanj <ryan.jarvinen@gmail.com>
2018-04-25 10:40:32 -0700

    moving to 8080

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+  port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
   redisCloud: {
     host: process.env.REDISCLOUD_URL,
